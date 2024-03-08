8416fc7845cf75603edd090cc3fb32e16b367e5f
Minh Le <duyminh.le310@gmail.com>
2024-03-07 21:23:10 -0500

    Add TODO comment for redirect of create-profile and edit-profile

--- src/app/create-profile/page.tsx
+++ src/app/create-profile/page.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import Form from "@/ui/profile-form";
 
 export default function CreateProfilePage() {
+  // Retrieve userId, check if user has already set up a profile,
+  // if so, redirect to edit profile page
+  // import { auth } from '@clerk/nextjs';
+  // const { userId } : { userId: string | null } = auth();
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Create Profile</h1>
--- src/app/edit-profile/page.tsx
+++ src/app/edit-profile/page.tsx
@@ -1,6 +1,11 @@
 import Form from "@/ui/profile-form";
 
 export default function EditProfilePage() {
+  // Retrieve userId, check if user hasn't set up a profile yet,
+  // if so, redirect to create profile page
+  // import { auth } from '@clerk/nextjs';
+  // const { userId } : { userId: string | null } = auth();
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Edit Profile</h1>
